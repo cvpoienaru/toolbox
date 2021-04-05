50bf10b0b9c6c3b42c1a449f17715db65549b193
Codrin-Victor Poienaru <cvpoienaru@gmail.com>
2021-04-05 12:38:38 -0700

    Added Wrench entry point

--- src/wrench.py
+++ src/wrench.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
 # SPDX-License-Identifier: BSD-2-Clause
 #
 # Copyright (c) 2021, Codrin-Victor Poienaru <cvpoienaru@gmail.com>.
 # All rights reserved.
 
-echo "Welcome to Wrench !"
+import sys
+
+if __name__ == "__main__":
+    print("Wrench was called with these args: ", str(sys.argv))
 
